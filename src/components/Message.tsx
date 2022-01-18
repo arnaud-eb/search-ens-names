164ab821b10f0c533ea21f224390af244da0e5e5
arnaud-eb <arnaud.depierreux@easebest.com>
2022-01-18 08:36:08 +0100

    Convert Message component to TypeScript

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import styled from "@emotion/styled/macro";
 
+import { ErrorType } from "../FormComponent";
+
+interface MessagePropsType {
+  alert: ErrorType;
+}
+
+interface MessageContainerPropsType {
+  type: string;
+}
+
 const MessageContainer = styled("article")`
   border-radius: var(--radius);
   padding: 5px 10px;
   overflow-wrap: break-word;
-  ${(p) =>
+  ${(p: MessageContainerPropsType) =>
     p.type === "success" &&
     `
     background-color: var(--clr-green-dark)
   `}
-  ${(p) =>
+  ${(p: MessageContainerPropsType) =>
     p.type === "error" &&
     `
     background-color: var(--clr-red-dark)
   `};
 `;
 
-const Message = ({ alert }) => {
+const Message = ({ alert }: MessagePropsType) => {
   console.log(alert);
   return (
     <MessageContainer type={alert.type}>
